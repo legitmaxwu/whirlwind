ebb9efe326a539ffaab2195ceac35c98801b54bf
Bao <baomai@berkeley.edu>
2023-05-05 14:24:16 -0700

    multi cw (package) setup

--- contracts/cw-juicer/src/bin/schema.rs
+++ contracts/cw-juicer/src/bin/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use whirlwind::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cw_juicer::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
