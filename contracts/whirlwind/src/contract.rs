11dd25509516bb9eff35b588ac6c22b70ca4e317
Bao <baomai@berkeley.edu>
2023-05-06 17:24:31 -0700

    finished instantiate

--- contracts/whirlwind/src/contract.rs
+++ contracts/whirlwind/src/contract.rs
@@ -4,22 +4,36 @@ use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult}
 // use cw2::set_contract_version;
 
 use crate::error::ContractError;
-use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::msg::{DenomUnvalidated, ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::state::{Denom, COMMITMENTS, DEPOSIT_AMOUNT, DEPOSIT_DENOM, VERIFIER};
 
-/*
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:whirlwind";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
-*/
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
     _info: MessageInfo,
-    _msg: InstantiateMsg,
+    msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    let denom = match msg.denom {
+        DenomUnvalidated::Native(denom) => Denom::Native(denom),
+        DenomUnvalidated::Cw20(addr) => Denom::Cw20(deps.api.addr_validate(&addr)?),
+    };
+    DEPOSIT_DENOM.save(deps.storage, &denom)?;
+    DEPOSIT_AMOUNT.save(deps.storage, &msg.amount)?;
+
+    let verifier = Verifier::new();
+    VERIFIER.save(deps.storage, &verifier)?;
+
+    let tree = MerkleTreeWithHistory::new(20);
+    COMMITMENTS.save(deps.storage, &tree)?;
+
+    Ok(Response::default())
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
--- contracts/whirlwind/src/state.rs
+++ contracts/whirlwind/src/state.rs
@@ -1,1 +1,18 @@
+use cw_storage_plus::{Item, Map, Addr};
+use lib::merkle_tree::MerkleTreeWithHistory;
+use lib::verifier::Verifier;
+use cw_serde::cw_serde;
 
+#[cw_serde]
+pub enum Denom {
+    Native(String)
+    Cw20(Addr)
+}
+
+pub const DEPOSIT_AMOUNT: Item<Uint128> = Item::new("deposit_amount");
+pub const DEPOSIT_DENOM: Item<Denom> = Item::new("deposit_denom");
+pub const VERIFIER: Item<Verifier> = Item::new("verifier");
+
+// Deposit
+pub const COMMITMENTS: Item<MerkleTreeWithHistory> = Item::new("commitments");
+pub const NULLIFIER_HASHES: Map<String, bool> = Map::new("nullifier_hashes");
