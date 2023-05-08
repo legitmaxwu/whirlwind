ee27fea62016964ec7b8f7451f03205c585c7447
Max Wu <chengmaxwu@gmail.com>
2023-05-08 05:47:06 -0700

    Cleanup repository

--- contracts/cw-juicer/src/state.rs
+++
@@ -1,8 +0,0 @@
-use cw_storage_plus::{Item, Map};
-use lib::merkle_tree::MerkleTreeWithHistory;
-use lib::verifier::Verifier;
-
-pub const COUNTER: Item<u64> = Item::new("counter");
-pub const VERIFIER: Item<Verifier> = Item::new("verifier");
-pub const COMMITMENTS: Item<MerkleTreeWithHistory> = Item::new("commitments");
-pub const NULLIFIER_HASHES: Map<String, bool> = Map::new("nullifier_hashes");
